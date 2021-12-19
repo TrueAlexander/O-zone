172a91370648fbfeeb4220193abb65b491eb3ab8
Alexander <caminante.msk@gmail.com>
2021-12-19 18:05:06 +0300

    O-zone / Day 3

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,5 +1,6 @@
-const getData = () => {
-  return fetch('https://test-project-f8ab0-default-rtdb.firebaseio.com/goods.json')
+const getData = (str) => {
+  return fetch(`https://test-project-f8ab0-default-rtdb.firebaseio.com/goods.json`)
+
     .then((response) => {
       return response.json();
     })
